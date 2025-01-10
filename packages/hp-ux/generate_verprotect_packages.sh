5d0e3edbcf7787bd70945dca1564903b4c5e3a22
hilmanzhy <hilmanzhy@gmail.com>
2025-01-10 16:06:29 +0700

    verprotect agent v4.10.0

--- packages/hp-ux/generate_verprotect_packages.sh
+++ packages/hp-ux/generate_verprotect_packages.sh
@@ -61,7 +61,7 @@ build_environment() {
     cd ${build_tools_path}
     mkdir bootstrap-gcc
     cd ${build_tools_path}/bootstrap-gcc
-    curl -k -SO http://packages.verprotect.com/utils/gcc/gcc_9.4_HPUX_build.tar.gz
+    curl -k -SO http://packages.wazuh.com/utils/gcc/gcc_9.4_HPUX_build.tar.gz
     gunzip gcc_9.4_HPUX_build.tar.gz
     tar -xf gcc_9.4_HPUX_build.tar
     rm -f gcc_9.4_HPUX_build.tar
@@ -69,7 +69,7 @@ build_environment() {
 
     # Install cmake 3.22.2
     cd ${build_tools_path}
-    curl -k -SO http://packages.verprotect.com/utils/cmake/cmake_3.22.2_HPUX_build.tar.gz
+    curl -k -SO http://packages.wazuh.com/utils/cmake/cmake_3.22.2_HPUX_build.tar.gz
     gunzip cmake_3.22.2_HPUX_build.tar.gz
     tar -xf cmake_3.22.2_HPUX_build.tar
     rm -f cmake_3.22.2_HPUX_build.tar
@@ -122,7 +122,7 @@ compile() {
     cd ${source_directory}/src
     config
     check_version
-    gmake deps RESOURCES_URL=http://packages.verprotect.com/deps/${deps_version} TARGET=agent
+    gmake deps RESOURCES_URL=http://packages.wazuh.com/deps/${deps_version} TARGET=agent
     gmake TARGET=agent USE_SELINUX=no
     bash ${source_directory}/install.sh
     # Install std libs needed to run the agent
--- packages/rpms/SPECS/verprotect-agent.spec
+++ packages/rpms/SPECS/verprotect-agent.spec
@@ -68,7 +68,7 @@ make clean
       MSGPACK="USE_MSGPACK_OPT=no"
     %endif
     deps_version=`cat Makefile | grep "DEPS_VERSION =" | cut -d " " -f 3`
-    make deps RESOURCES_URL=http://packages.verprotect.com/deps/${deps_version} TARGET=agent
+    make deps RESOURCES_URL=http://packages.wazuh.com/deps/${deps_version} TARGET=agent
     make -j%{_threads} TARGET=agent USE_AUDIT=no USE_SELINUX=yes USE_EXEC_ENVIRON=no DEBUG=%{_debugenabled} ${MSGPACK}
 
 %endif
